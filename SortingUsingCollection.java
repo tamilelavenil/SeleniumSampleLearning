50b05790fac7be6c6f7be7dee9c86951c7039fa0
tamilelavenil <93430431+tamilelavenil@users.noreply.github.com>
2021-11-19 00:45:13 +0530

    Create SortingUsingCollection.java

---
+++ SortingUsingCollection.java
@@ -0,0 +1,16 @@
+//package week3.day2;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+
+//public class SortingUsingCollection {
+//	String[] input = {"HCL","Wipro","Aspire Systems","CTS"};
+//	List<String> values = new ArrayList();
+//	public static void main(String[] args) {
+//		for (String data : input) {
+//			values.add(data);
+//		}
+//		System.out.println(values);
+//	}
+//
+//}
